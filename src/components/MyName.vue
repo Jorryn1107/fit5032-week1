a77287be754dfbdb5d8c45ae49fff1f25fb28576
jiaying <jlin0158@student.monash.edu>
2024-07-28 00:07:04 +1000

    first commit

---
+++ src/components/MyName.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <p> Welcome, {{ name }}! </p>
+    </div>    
+</template>
+
+<script>
+export default {
+    name: 'MyName',
+    props: {
+        name: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+p {
+    font-weight: 1000;
+    font-size: 1.5rem;
+    color: #5e1010;
+}
+</style>
